ddfa2a4179c1cabb2187b5c3fbd7ac4a681e2517
Tris Colaco <triscolaco28@gmail.com>
2023-03-09 19:45:01 -0500

    factorial version 1.1

--- factorials.py
+++ factorials.py
@@ -1,10 +1,20 @@
 def factorial(number):
-    value = number # for the print statement
-    fact = number  # assigning variable number to variable fact
-    # Mistake code: while number <= 1:
-    while number > 1: # while loop for number decrementing by one until greater than 1
-        number = number - 1
-        fact  = fact * number # multiply variable fact to decremented number variable and store it in fact
-    print("The factorial for",value, "is", fact)
+    if type(number) is not int: # if number is not a integer
+        return None
+    if number < 0: # if number is negative
+        return None
+    if number == 0: # if number is 0
+        return 1
+    if number > 0:
+        fact = number  # assigning variable number to variable fact
+        # Mistake code: while number <= 1:
+        while number > 1: # while loop for number decrementing by one until greater than 1
+            number = number - 1
+            fact  = fact * number # multiply variable fact to decremented number variable and store it in fact
+        return fact
 
-factorial(5)
+print(factorial(5))
+print(factorial(3))
+print(factorial(-2))
+print(factorial('spam spam spam spam spam'))
+print(factorial(0))
